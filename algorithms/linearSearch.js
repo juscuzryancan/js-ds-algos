c03ab0aa4ebc901036ee193207fd3fe6828f6ef7
Ryan Riley Puzon <rrgpuzon@gmail.com>
2022-11-23 10:09:04 -0800

    implements linear search

---
+++ algorithms/linearSearch.js
@@ -0,0 +1,23 @@
+/**
+ * Linear Search goes through each element within an input array from start to end
+ * to see if the target is present within the array
+ * Javascript impl can return null when the target is not found
+ * Runtime: O(n)
+ * Space Complexity: O(1) no additional space needed
+ */
+const linearSearch = (array, target) => {
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === target) {
+      return target;
+    }
+  }
+
+  return null;
+}
+
+//Example Calls
+const array = [1, 2, 3, 4, 5];
+console.log(linearSearch(array, 3));
+console.log(linearSearch(array, 10));
+
+module.export = linearSearch;
